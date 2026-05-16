90d974bc63c640fc79c3d7e7e94aa6e7875f830e
Durgesh kumar <Kumardurgeshjha398@gmail.com>
2026-05-16 20:05:37 +0530

    first commit

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
+    port: process.env.SMTP_PORT,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
